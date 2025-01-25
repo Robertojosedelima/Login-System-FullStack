0f30f2b4d4d7ddd135c270d8ae62cc831bee1b7d
Roberto lima <drop1108@gmail.com>
2025-01-25 15:55:40 -0300

    Starter project .

---
+++ src/main/java/br/com/alphacodebrasil/login_system_fullstack/LoginSystemFullstackApplication.java
@@ -0,0 +1,13 @@
+package br.com.alphacodebrasil.login_system_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSystemFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSystemFullstackApplication.class, args);
+	}
+
+}
